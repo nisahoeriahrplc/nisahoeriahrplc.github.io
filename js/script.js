e86e85321209fed4a86d5607f1a09b89fc70d20d
nisahoeriahrplc <93115619+nisahoeriahrplc@users.noreply.github.com>
2021-10-25 18:34:51 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,3 +13,48 @@ $('.page-scroll').on('click', function(e){
 
    e.preventDefault();
 });
+
+
+
+
+// parallax
+//about
+$(window).on('load', function() {
+    $('.pKiri').addClass('pMuncul');
+    $('.pKanan').addClass('pMuncul');
+});
+
+
+$(window).scroll(function() {
+    var wScroll = $(this).scrollTop();
+    
+    // jumbotron
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    });
+
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    });
+
+
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+    });
+
+
+    //portfolio
+    if(wScroll > $('.portfolio').offset().top - 250 ) {
+        $('.portfolio .thumbnail').each(function(i) {
+            setTimeout(function() {
+                $('.portfolio .thumbnail').eq(i).addClass('muncul'); 
+            }, 300 * (i+1));
+        });
+      
+      
+       
+    }
+
+
+});
